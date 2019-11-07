929bed722dc358aae7a2d436a41dc162f6002274
Oliver <oliverjgodoy@gmail.com>
2019-11-07 18:09:05 -0300

    teste de rotas com o modulo homePageController

--- routes.js
+++ routes.js
@@ -1,3 +1,7 @@
 const express = require('express')
 const route = express.Router() //Assim pode fazer o roteamento
+const homePageController = require('./controllers/homePageController')//Importando o modulo da pasta controllers
+module.exports = route //exportando o route, que contem todas as rotas, para o modulo server.js importa-lo
 
+
+route.get('/', homePageController.funcaoHomePage)
--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require('express')
 const app = express()
 app.use(express.urlencoded({extended: true})) //Definir o conteúdo de request, usando req.body
-
+const routes = require('./routes') //importando o modulo routes.js
+app.use(routes) //Faz com que o express use as suas rotas
 
 
 //Servidor sendo executado na porta 3001 localhost.
