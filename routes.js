4a3cd8cbdffec17c20774fb1972a253bee44a02f
Oliver <oliverjgodoy@gmail.com>
2019-12-03 09:30:32 -0300

    Login page

--- routes.js
+++ routes.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const route = express.Router() //Assim pode fazer o roteamento
 
+const homePageController = require('./src/controllers/homePageController')//Importando os modulos da pasta controllers
+const loginPageController = require('./src/controllers/loginPageController')
+
 module.exports = route //exportando o route, que contem todas as rotas, para o modulo server.js importa-lo
 
-//Importando os modulos da pasta controllers
-const homePageController = require('./src/controllers/homePageController')
-
 //usando as funções dos modulos. Rotas CRUD
+
 //Rotas home
-route.get('/', homePageController.funcaoHomePage)
+route.get('/', homePageController.index)
+
+//Rotas login
+route.get('/login', loginPageController.index)
--- src/controllers/homePageController.js
+++ src/controllers/homePageController.js
@@ -1,12 +1,4 @@
-const TesteModel = require('../models/TesteModel')
-
-TesteModel.create({
-    titulo: 'Título teste'
-})
-.then(dados => console.log(dados))
-.catch(e => console.log(e))
-
-exports.funcaoHomePage = (req, res) => { //exportando a função "funcaoHomePage"
+exports.index = (req, res) => { //exportando a função "funcaoHomePage"
     res.render('index') 
     return
 }
